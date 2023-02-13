ac89832502976ab26415dafd1e0cfcbeece4fd19
4thdraw <4thdraw2@gmail.com>
2023-02-13 16:38:40 +0900

    ddd

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,7 +1,9 @@
 import * as React from 'react';
+import { useState } from 'react';
 import Select from "react-select";
 import { useForm , Controller} from "react-hook-form";
 import { Checkbox, FormControlLabel, RadioGroup, Radio } from "@material-ui/core";
+import axios from "axios";
 
 
 
@@ -11,6 +13,8 @@ function Contact(props) {
   const styleValue = ['html_css','jQuery','ECMA6','react','node','php'];
   const radioValue = ['파트타임','하루종일','딱 8시간'];
 
+  const [loading, setLoading] = useState(false);
+
   const { register, handleSubmit, control, formState: { errors } } = useForm({
     defaultValues: {
       company: '',
@@ -18,13 +22,32 @@ function Contact(props) {
       gender: '',
       age : '',
       ability: [],      
-      worktime: radioValue[0]
+      worktime: radioValue[0],
+      select :{ value: "chocolate", label: "Chocolate" }
     }
   })
-
+//sql 쿼리문으로 서버(리액트가 아닌 노드가)에서 들어갈 수 있도록 처리해야한다.
+// 리액트는 그저 이 정보를 서버에 전달만 하고 결과만 받으면 됨
+//   INSERT INTO `bby_contact` (`id` , `company`, `email`, `gender`, `age`, `ability`, `worktime`, `select` )
+//   VALUE (null, '회사이름','4thdraw@naver.com','f','50', '["html_css","php"]', '파트타임', '{"value":"strawberry","label":"Strawberry"}')
   
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
+    setLoading(true);
     alert(JSON.stringify(data));
+
+    // try {
+    //     const response = await axios.post("노드서버라우트", {
+    //       email,
+    //       name,
+    //       userpwd,
+    //       phone,
+    //     });
+    //     console.log(response);
+    //   } catch (error) {
+    //     alert(error.response.data);
+    // }
+
+    setLoading(false);
   };
 
 
@@ -44,13 +67,13 @@ function Contact(props) {
                             <span>회사명</span>
                             <input {...register("company" ,  {
                                     required: true,
-                                    message: "회사명은 필수 입력입니다.",
+                                    
                                     }) } />
-                            {errors.company && <p role="alert">회사명 필수임</p>}
+                            {errors.company && <p role="alert">회사이름을 기입해주세요</p>}
                         </li>
                         <li>
                             <span>이메일</span><input {...register("email" ,  {
-                            required: true,
+                            required: '이메일을 넣어주세요',
                             pattern: {
                             value: /\S+@\S+\.\S+/,
                             message: "이메일 형식에 맞지 않습니다.",
@@ -61,9 +84,9 @@ function Contact(props) {
                         <li>
                             <span>하나선택</span>
                             <select {...register("gender", {required: true, message: "꼭 하나선택하기" })}>
-                                <option value="female">female</option>
-                                <option value="male">male</option>
-                                <option value="other">other</option>
+                                <option value="f">female</option>
+                                <option value="m">male</option>
+                                <option value="o">other</option>
                             </select>
                             {errors.gender && <p role="alert">{errors.gender.message}</p>}
                         </li>
--- src/component/Preinterview.js
+++ src/component/Preinterview.js
@@ -1,28 +1,71 @@
-import interviewDBlink from '../json/preinterview.json';
+////import interviewDBlink from '../json/preinterview.json';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+
 const Preinterview = (props) => {
+  const [faqlist, setfaqlist] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
     
-    const interviewjson = interviewDBlink[props.objnm];   
+   //// const interviewjson = interviewDBlink[props.objnm]; 
+
+    const fetchFaq = async () => {
+      try {
+         // 요청이 시작 할 때에는 error 와 users 를 초기화하고
+      setError(null);
+      setfaqlist(null);
+      // loading 상태를 true 로 바꿉니다.
+      setLoading(true);
+      const response = await axios.get(
+        '//node14.cafe24app.com/notice?type=list'
+      );
+      setfaqlist( response.data );
+      
+
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false); //응답받고 랜더링
+      
+    }
+
+
+    useEffect(() => {
+      fetchFaq(); //마우팅끝나고 바로 한번만 요청실행
+    }, []);
+    
+   
    
     return(
       
       <section className="section py-5 text-center" id={props.contentid}>
-          <h3>{props.title}</h3>
-          <div className="py-5 container-md text-start">
-            <ul>
-               {
-                interviewjson.map(function(value, index){
-                    //this조심할것
-                    return(
-                      <li  key={'interview'+index}>
-                        <strong className='d-block border-bottom py-2 ' role="button" onClick={ e => { console.log( e.target.nextSibling.classList.toggle('d-none') ) }}>{value.subject}</strong>
-                        <p className='py-5 d-none'>{value.content}</p>
-                      </li>
-                    )
-                }
-                )
-               }
-            </ul>
-          </div>
+          {   loading && <div>로딩중..</div> }
+          { error &&  <div>에러가 발생했습니다</div>}
+          {
+            loading === false && <div>
+                                  <h3>{props.title}</h3>
+                                    <div className="py-5 container-md text-start">
+                                      <ul>
+                                        {
+                                          console.log(faqlist)
+                                          // interviewjson.map(function(value, index){
+                                          //     //this조심할것
+                                          //     return(
+                                          //       <li  key={'interview'+index}>
+                                          //         <strong className='d-block border-bottom py-2 ' role="button" onClick={ e => { console.log( e.target.nextSibling.classList.toggle('d-none') ) }}>{value.subject}</strong>
+                                          //         <p className='py-5 d-none'>{value.content}</p>
+                                          //       </li>
+                                          //     )
+                                          // }
+                                          // )
+                                        }
+                                      </ul>
+                                    </div>
+                                  </div>
+          }
+          
       </section>
     )
   }
